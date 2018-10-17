249fe93c7c329f34b005c16695a286605fa7a159
Andrii Samoilenko <andrii@Macbook-Pro-as.local>
2018-10-17 10:30:07 +0300

    jquery

--- js/jq.js
+++ js/jq.js
@@ -1,9 +1,28 @@
-jQuery(function(){
-    $(window).on('load', function () {
+jQuery(document).ready(function(){
 
-        $('#preloader-area').delay(350).fadeOut('slow');
+    $("#preloader-area").delay(400).fadeOut(400);
 
 
-        
+$(window).scroll(function(){
+    if($(window).scrollTop() >150){
+        $('.header-top-area').addClass('header-top-area-scroll');
+    }
+    else{
+        $('.header-top-area').removeClass('header-top-area-scroll')
+    }
 })
+
+console.log($('.header-top-area ul').html());
+
+
+$('.header-top-area ul li a').on("click",function(e){
+   var azzz = $(this);
+
+  $('html','body').stop().animate({
+    scrollTop: $(azzz.attr('href')).offset().top - 50
+        },1000);
+        e.preventDefault();
+})
+
+
 })
